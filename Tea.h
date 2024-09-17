c4575b748988f53d0891b9a0ae7596b57f2fa074
Benjamin Alexander <146328131+Benjameepmeep@users.noreply.github.com>
2024-09-17 12:10:52 +0200

    Tea.h in progress : )

---
+++ Tea.h
@@ -0,0 +1,44 @@
+#pragma once
+
+struct TeaIngredients
+{
+	bool tea;
+	bool honey;
+	int waterAmount;
+	int cupSize;
+};
+void PrepareItems(TeaIngredients teaItems)
+{
+	if (!teaItems.tea || teaItems.waterAmount >= teaItems.cupSize)
+	{
+
+	}
+}
+int HeatWater(bool tea)
+{
+
+}
+int PourTea(bool overITStaff = true, int remainingWater)
+{
+	// Would you like to pour remaining water over IT staff? Your internet is failing.
+}
+int SteepTea()
+{
+
+}
+void StirTea()
+{
+
+}
+void RemoveWasteFromCup()
+{
+
+}
+bool DrinkTea()
+{
+
+}
+int AddIngredients()
+{
+
+}
